e4efb89e62136b1961c2afa760de387e7d214f35
mmruano <146729468+mmruano@users.noreply.github.com>
2024-01-26 16:30:19 +0100

    First commit of the created API in Springboot.

---
+++ src/main/java/com/vedruna/twitterapi2ev/Twitterapi2evApplication.java
@@ -0,0 +1,13 @@
+package com.vedruna.twitterapi2ev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Twitterapi2evApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Twitterapi2evApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vedruna/twitterapi2ev/Twitterapi2evApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedruna.twitterapi2ev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Twitterapi2evApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
